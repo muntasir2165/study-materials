9700bd1e57510cc325715970ad77de88731e53c0
mstop4 <mstop4@gmail.com>
2018-07-24 21:39:12 -0400

    added week 10 days 2 and 3 solutions

--- 10-nodejs/02-Day/14-AppendFile/appendFile.js
+++ 10-nodejs/02-Day/14-AppendFile/appendFile.js
@@ -1,14 +1,14 @@
-// As always, we grab the fs package to handle read/write
+// As always, we grab the fs package to handle read/write.
 var fs = require("fs");
 
-// We then store the textfile filename given to us from the command line
+// Next, we store the text file filename given to us from the command line.
 var textFile = process.argv[2];
 
-// We then append the contents "Hello Kitty" into the file
-// If the file didn't exist then it gets created on the fly.
+// Next, we append the contents "Hello Kitty" into the file.
+// If the file didn't exist, then it gets created on the fly.
 fs.appendFile(textFile, "Hello Kitty", function(err) {
 
-  // If an error was experienced we say it.
+  // If an error was experienced we will log it.
   if (err) {
     console.log(err);
   }
@@ -19,5 +19,3 @@ fs.appendFile(textFile, "Hello Kitty", function(err) {
   }
 
 });
-
-
